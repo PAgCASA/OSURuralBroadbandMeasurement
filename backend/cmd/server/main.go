6d5235534cd83bc70f0cecf9b07665ea32f4ba04
Yunpeng Wen <62644206+CrystalSou1@users.noreply.github.com>
2022-01-28 20:25:07 -0800

    Add initial speed test results API (#4)

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/go-sql-driver/mysql"
 	"github.com/gofiber/fiber/v2"
@@ -59,6 +61,68 @@ func submitSpeedTest(c *fiber.Ctx) error {
 	// return OK to client if valid, else return error.
 	// Note: the struct should be named "SpeedTestResult"
 
-	c.Response().AppendBodyString("Not Implemented Yet")
-	return nil
+	type SpeedTestResultOriginal struct {
+		AndroidID     string `json:"androidID"`
+		IphoneXSID    string `json:"iphoneXSID"`
+		IphoneID      string `json:"iphoneID"`
+		PhoneID       string `json:"phoneID"`
+		TestID        string `json:"testID"`
+		DownloadSpeed string `json:"downloadSpeed"`
+		UploadSpeed   string `json:"uploadSpeed"`
+		Latency       string `json:"latency"`
+		Jitter        string `json:"jitter"`
+		PacketLoss    string `json:"packetLoss"`
+	}
+	type SpeedTestResult struct {
+		PhoneID       string
+		TestID        string
+		DownloadSpeed int
+		UploadSpeed   int
+		Latency       int
+		Jitter        int
+		PacketLoss    int
+	}
+	o := new(SpeedTestResultOriginal)
+	if err := c.BodyParser(o); err != nil {
+		return err
+	}
+	var r SpeedTestResult
+	do, err := strconv.Atoi(o.DownloadSpeed)
+	up, err := strconv.Atoi(o.UploadSpeed)
+	la, err := strconv.Atoi(o.Latency)
+	ji, err := strconv.Atoi(o.Jitter)
+	pa, err := strconv.Atoi(o.PacketLoss)
+	if err != nil {
+		fmt.Printf("%v ", o.DownloadSpeed)
+	}
+	r.PhoneID = o.AndroidID + o.IphoneXSID + o.IphoneID + o.PhoneID
+	r.TestID = o.TestID
+	r.DownloadSpeed = do
+	r.UploadSpeed = up
+	r.Latency = la
+	r.Jitter = ji
+	r.PacketLoss = pa
+
+	log.Println("PhoneID: ", r.PhoneID)
+	log.Println("TestID: ", r.TestID)
+	log.Println("DownloadSpeed: ", r.DownloadSpeed)
+	log.Println("UploadSpeed: ", r.UploadSpeed)
+	log.Println("Latency: ", r.Latency)
+	log.Println("Jitter: ", r.Jitter)
+	log.Println("PacketLoss: ", r.PacketLoss)
+	if r.DownloadSpeed < 1 {
+		c.Response().AppendBodyString("Invalid value for downloadSpeed")
+		return c.SendStatus(400)
+	}
+	if r.UploadSpeed < 1 {
+		c.Response().AppendBodyString("Invalid value for uploadSpeed")
+		return c.SendStatus(400)
+	}
+	if r.PacketLoss > 100 {
+		c.Response().AppendBodyString("packetLoss must be less than 100")
+		return c.SendStatus(400)
+	}
+
+	c.Response().AppendBodyString("OK")
+	return c.SendStatus(200)
 }
