ac3b3d23e2ebef74ad90413d788a196d7df44122
Ethan Hampton <etho@ethohampton.com>
2022-06-02 21:58:58 -0700

    Make a few small changes to the schema

--- database/initialSchema.sql
+++ database/initialSchema.sql
@@ -2,19 +2,19 @@ CREATE TABLE IF NOT EXISTS SpeedTests (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     testID TEXT NOT NULL,
     phoneID TEXT NOT NULL,
-    uploadSpeed DECIMAL NOT NULL,
-    downloadSpeed DECIMAL NOT NULL,
+    uploadSpeed DECIMAL(9, 2) NOT NULL,
+    downloadSpeed DECIMAL(9, 2) NOT NULL,
     latency INTEGER NOT NULL, -- in milliseconds
-    jitter DECIMAL NOT NULL, -- in milliseconds
-    packetLoss DECIMAL, -- in percentage (0-100), can be null if not run
+    jitter DECIMAL(9, 2) NOT NULL, -- in milliseconds
+    packetLoss DECIMAL(9, 2), -- in percentage (0-100), can be null if not run
     
     testStartTime DATETIME NOT NULL, -- in UTC, time the test started
     testDuration INTEGER NOT NULL, -- in milliseconds
     recievedTime DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    longitude DECIMAL NOT NULL, -- location data
-    latitude DECIMAL NOT NULL,
-    accuracy DECIMAL NOT NULL -- in meters
+    longitude DECIMAL(8,6) NOT NULL, -- location data
+    latitude DECIMAL(9,6) NOT NULL,
+    accuracy DECIMAL(9, 2) NOT NULL -- in meters
 );
 
 CREATE TABLE IF NOT EXISTS PersonalData (
